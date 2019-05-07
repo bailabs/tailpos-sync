35903c020d2a9b7a7e54faba0f180a4c72c70b17
Ivan Ray Altomera <irayspacii@gmail.com>
2019-05-07 15:53:16 +0800

    refactor: removed unused doctypes and functions

--- tailpos_sync/tailpos_sync/doctype/revision_hashes/revision_hashes.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2018, Bai Web and Mobile Lab and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Revision Hashes', {
-	refresh: function(frm) {
-
-	}
-});
