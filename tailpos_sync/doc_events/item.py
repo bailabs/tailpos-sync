2b1e2f7f3a8012e7fcafe79c657e1e5662785442
Ivan Ray Altomera <irayspacii@gmail.com>
2019-07-29 01:17:09 +0800

    refactor: use utils for item doc_events

--- tailpos_sync/doc_events/item.py
+++ tailpos_sync/doc_events/item.py
@@ -1,11 +1,10 @@
-import uuid
+from tailpos_sync.utils import set_date_updated, set_doc_id
 
 
 def validate(doc, method):
-    if doc.date_updated is None:
-        doc.date_updated = doc.modified
+    set_date_updated(doc)
 
 
 def before_save(doc, method):
-    if doc.in_tailpos and not doc.id:
-        doc.id = str(uuid.uuid4())
+    if doc.in_tailpos:
+        set_doc_id(doc)
