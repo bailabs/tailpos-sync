1632a15e529361a9fa2c08e6592bbf41e859c31b
jiloysss <jangeles@bai.ph>
2019-11-13 09:32:49 +0800

    Multiple Payment Types

---
+++ tailpos_sync/config/tailpos_sync.py
@@ -0,0 +1,54 @@
+# import frappe
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Payroll"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Attendancts",
+				},
+                {
+                    "type": "doctype",
+                    "name": "Item",
+                },
+                {
+                    "type": "doctype",
+                    "name": "Categories",
+                },
+                {
+                    "type": "doctype",
+                    "name": "Discounts",
+                },
+                {
+                    "type": "doctype",
+                    "name": "Receipts",
+                },
+                {
+                    "type": "doctype",
+                    "name": "Payments",
+                },
+                {
+                    "type": "doctype",
+                    "name": "Shifts",
+                },
+                {
+                    "type": "doctype",
+                    "name": "Device",
+                },
+                {
+                    "type": "doctype",
+                    "name": "Tail Settings",
+                },
+                {
+                    "type": "doctype",
+                    "name": "Wallet",
+                },
+                {
+                    "type": "doctype",
+                    "name": "Wallet Logs",
+                }
+			]
+		}]
