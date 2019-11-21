fdcacab7efba232b01b8e445d0089c9550d54f78
Ivan Ray Altomera <irayspacii@gmail.com>
2019-11-21 17:09:11 +0800

    feat(api.py): Item Groups filter from Device

--- tailpos_sync/api.py
+++ tailpos_sync/api.py
@@ -2,25 +2,24 @@
 # Functions used by TailOrder
 # ---
 import frappe
-import json
+# import json
+
+from frappe import _
+from toolz import compose, partial, pluck
 
 
 @frappe.whitelist(allow_guest=True)
 def fetch_items():
-    try:
-        data = json.loads(frappe.request.data)
-    except:
-        data = None
-
-    pos_profile = None
-
-    if data:
-        pos_profile = frappe.db.get_value('Device', data['device'], 'pos_profile')
-
     use_price_list = frappe.db.get_single_value('Tail Settings', 'use_price_list')
 
     if use_price_list:
-        items = get_items_with_price_list_rate(pos_profile)
+        data = frappe.local.form_dict
+        device = _validate_device(data.get('device'))
+        pos_profile = frappe.db.get_value('Device', device, 'pos_profile')
+        items = get_items_with_price_list_rate(
+            pos_profile,
+            _get_item_groups_filter(device)
+        )
     else:
         items = get_items_with_standard_rate()
 
@@ -37,15 +36,32 @@ def fetch_remarks():
     return frappe.get_all('Remarks Template', fields=['name'])
 
 
-def get_items_with_price_list_rate(pos_profile=None):
+def get_items_with_price_list_rate(pos_profile=None, item_groups=None):
     if not pos_profile:
         pos_profile = frappe.db.get_single_value('Tail Settings', 'pos_profile')
 
     price_list = frappe.db.get_value('POS Profile', pos_profile, 'selling_price_list')
 
-    items = frappe.db.sql("""SELECT `tabItem`.name, `tabItem`.category, `tabItem`.item_name, `tabItem Price`.price_list_rate as standard_rate, `tabItem`.color FROM `tabItem` INNER JOIN `tabItem Price` ON `tabItem`.name = `tabItem Price`.item_code WHERE `tabItem`.in_tailpos = 1 AND `tabItem Price`.price_list=%s""", price_list, as_dict=True)
+    if item_groups:
+        get_filter = compose(
+            ', '.join,
+            partial(map, lambda item_group: '\'%s\'' % item_group)
+        )
+        item_groups_query = 'AND `tabItem`.item_group IN (%s)' % get_filter(item_groups)
 
-    return items
+    return frappe.db.sql("""
+        SELECT 
+            `tabItem`.name,
+            `tabItem`.category,
+            `tabItem`.item_name,
+            `tabItem Price`.price_list_rate as standard_rate,
+            `tabItem`.color
+        FROM `tabItem` 
+        INNER JOIN `tabItem Price` ON `tabItem`.name = `tabItem Price`.item_code 
+        WHERE `tabItem`.in_tailpos = 1 
+        AND `tabItem Price`.price_list = '{price_list}'
+        {item_groups}
+    """.format(price_list=price_list, item_groups=item_groups_query), as_dict=1)
 
 
 def get_items_with_standard_rate():
@@ -74,3 +90,15 @@ def camelized_element(index, element):
     if index == 0:
         return element.lower()
     return element.capitalize()
+
+
+def _validate_device(device):
+    if not frappe.db.exists('Device', device):
+        frappe.throw(_('Device not found'))
+    return device
+
+
+def _get_item_groups_filter(device):
+    return compose(list, partial(pluck, 'item_group'))(
+        frappe.get_all('Device Item Group', filters={'parent': device}, fields=['item_group'])
+    )
